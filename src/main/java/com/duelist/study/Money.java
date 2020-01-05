63332be3636b95e9ee3693000b372acf02a9df1f
Duelist256 <selykov.iw@yandex.ru>
2020-01-05 19:02:56 +0300

    Add currency, refactoring

--- src/main/java/com/duelist/study/Money.java
+++ src/main/java/com/duelist/study/Money.java
@@ -1,11 +1,32 @@
 package com.duelist.study;
 
-public class Money {
+public abstract class Money {
+
     protected int amount;
+    protected String currency;
+
+    abstract Money times(int multiplier);
+
+    static Money dollar(int amount) {
+        return new Dollar(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount, "CHF");
+    }
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
 
     @Override
     public boolean equals(Object obj) {
         Money money = (Money) obj;
-        return amount == money.amount;
+        return amount == money.amount && this.getClass() == obj.getClass();
     }
 }
