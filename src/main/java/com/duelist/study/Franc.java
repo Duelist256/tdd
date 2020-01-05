c991202f1dd31ce7f384ec5861ff53d0163dd6ab
Duelist256 <selykov.iw@yandex.ru>
2020-01-05 17:41:50 +0300

    Add Dollar and Franc

---
+++ src/main/java/com/duelist/study/Franc.java
@@ -0,0 +1,12 @@
+package com.duelist.study;
+
+public class Franc extends Money {
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+}
