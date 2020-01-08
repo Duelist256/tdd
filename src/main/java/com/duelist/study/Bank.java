54394e62e7cb262255f02484f91b551840fec4a5
Duelist256 <selykov.iw@yandex.ru>
2020-01-08 16:28:07 +0300

    Add tests and refactoring

--- src/main/java/com/duelist/study/Bank.java
+++ src/main/java/com/duelist/study/Bank.java
@@ -1,7 +1,43 @@
 package com.duelist.study;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable<Bank.Pair, Integer> rates = new Hashtable<Bank.Pair, Integer>();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate = rates.get(new Pair(from, to));
+        return rate;
+    }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Pair pair = (Pair) obj;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return 0;
+        }
     }
 }
